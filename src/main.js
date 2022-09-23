da210b35e41abf6b0fa4c860ff645682daaf7901
Melodie Xinyun Jin <melodiej@umich.edu>
2022-09-23 11:13:07 -0400

    add Vue-Router and moving About page into separate file component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import './gallery.css'
 import App from './App.vue'
+import About from './pages/About.vue'
+// import Home from './pages/App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: About },
+  // { path: '/about', component: About },
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
