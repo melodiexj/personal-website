bf8ce3a42daf6a060da6322cef1217ff9249a86f
Melodie Xinyun Jin <melodiej@umich.edu>
2022-09-22 23:09:06 -0400

    implemented gallery of images on about page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <script setup>
 
+  // TODO: later, get this data from external database
+  const galleryImages = [
+    {
+      url: "/src/assets/gallery/Children_of_the_Sky.png",
+      header: "Scene from 'Children of the Sky'",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?",
+    },
+    {
+      url: "/src/assets/gallery/moon_over_the_mountains.png",
+      header: "Study: contrasting colors",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?",
+    },
+    {
+      url: "/src/assets/gallery/peach_sky_mountains.png",
+      header: "Study: soft/hard edges",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?",
+    },
+    {
+      url: "/src/assets/gallery/sunset.png",
+      header: "Study: Color transition",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?",
+    },
+  ]
 
 </script>
 
@@ -16,6 +39,23 @@
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?</p>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?</p>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas placeat dolorum atque, at quidem reiciendis velit alias inventore fuga earum unde eaque ipsa ad. Maxime animi quod optio! Ullam, aperiam?</p>
+  </section>
+
+  <section id="about-me-gallery">
+    <header>
+      <h2>Check out some of my work!</h2>
+    </header>
+
+    <div class="gallery-small">
+
+      <div v-for="imgInfo in galleryImages" :key="imgInfo['header']" class="gallery-item">
+        <img :src="imgInfo['url']"/>
+
+        <h3>{{imgInfo['header']}}</h3>
+        <p>{{imgInfo['description']}}</p>
+      </div>
+
+    </div>
   </section>
 
 </template>
