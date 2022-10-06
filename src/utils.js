5f47edf81306def046fe2af76655256fe45dc2dc
Melodie Xinyun Jin <melodiej@umich.edu>
2022-10-05 23:22:34 -0400

    fixed fetching of json data (cause: incorrect root/url root)

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,18 @@
 const fetchData = async (url, attr) => {
-  const d = await(await fetch(url))
-  .json()
+  // const d = await(await fetch(url))
+  // .json()
+  // .catch(e => {
+  //   console.error(e);
+  // });
+
+  // return attr ? d[attr] : d;
+
+  return await fetch("/" + url)
+  .then((response) => response.json())
+  .then((data) => data[attr])
   .catch(e => {
     console.error(e);
   });
-
-  return attr ? d[attr] : d;
 }
 
 export {
